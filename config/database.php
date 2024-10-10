30bd2a26e8db5b5736f55648f82ef2bb8fc885f1
chuc-chuc <60476455+chuc-chuc@users.noreply.github.com>
2024-10-10 17:07:07 -0600

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,16 +1,14 @@
 <?php
-function obtenerConexionDB() {
+function db()
+{
     $servidor = "localhost";
-    $usuario = "root";
-    $password = "";
-    $db = "fincoop";
-
+    $usuario = "u542351893_fin";
+    $password = "Chuc1234#";
+    $db = "u542351893_fin";
     $conexion = new mysqli($servidor, $usuario, $password, $db);
-
     if ($conexion->connect_error) {
-        die("Conexión fallida: " . $conexion->connect_error);
+        die("Conexión fallida jajaja: " . $conexion->connect_error);
     }
-
     return $conexion;
 }
-?>
+
